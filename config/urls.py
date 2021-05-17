9e33ec5b77a4fddef1ad052fd7ecd113106cc333
Allan Smiff <allandifere@gmail.com>
2021-05-17 16:37:04 -0300

    concluded

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from dashboard.views import index
 from visitantes.views import (
@@ -18,6 +19,20 @@ urlpatterns = [
          index,
          name="index",
          ),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name = "login.html"
+        ),
+        name ="login"
+    ),
+    path(
+        "logout",
+        auth_views.LogoutView.as_view(
+            template_name ="logout.html"
+        ),
+        name="logout"
+    ),
 
     path(
         "registrar-visitante/",#aparece na url do navegador
